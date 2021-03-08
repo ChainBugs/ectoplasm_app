4f0c778e6b494bbb9bfbb53a180197f1ef9f0dcd
Marcus Kalmar <marcus.kalmar@gmail.com>
2021-03-08 19:06:53 +0100

    set up elementary screen logic

---
+++ skeleton_project/lib/screens/home/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return HomeScreenContent();
+  }
+}
+
+class HomeScreenContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
