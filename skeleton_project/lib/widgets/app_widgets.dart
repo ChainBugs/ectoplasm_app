ce69a8ca26842aa0c57cc614114dea444ee7cfa4
Marcus Kalmar <marcus.kalmar@gmail.com>
2021-03-08 19:04:20 +0100

    set up fundamental architecture

---
+++ skeleton_project/lib/widgets/app_widgets.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppButton extends StatelessWidget {
+  final String text;
+  final Function onPressed;
+  final ButtonStyle style;
+
+  AppButton({this.text = "Click me!", this.onPressed, this.style});
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Text(text),
+      onPressed: () => onPressed,
+      style: style,
+    );
+  }
+}
