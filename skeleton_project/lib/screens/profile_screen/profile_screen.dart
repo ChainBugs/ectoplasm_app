110bf2ba2ba622e0f48c730452e7a57672800d9e
Marcus Kalmar <marcus.kalmar@gmail.com>
2021-03-09 22:52:10 +0100

    make a skeleton profile screen and change theme to indigo

--- skeleton_project/lib/screens/profile_screen/profile_screen.dart
+++ skeleton_project/lib/screens/profile_screen/profile_screen.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skeleton_project/screens/profile_screen/bloc/profile_bloc.dart';
 
 class ProfileScreen extends StatelessWidget {
   static const routeName = "/profile_screen";
   @override
   Widget build(BuildContext context) {
-    return ProfileScreenContent();
+    return BlocProvider<ProfileBloc>(
+      create: (context) => ProfileBloc(),
+      child: _ProfileScreenContent(),
+    );
   }
 }
 
-class ProfileScreenContent extends StatelessWidget {
+class _ProfileScreenContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("The Profile Screen"),
+      ),
+      body: BlocListener<ProfileBloc, ProfileState>(
+        listener: (context, state) {},
+        child: BlocBuilder<ProfileBloc, ProfileState>(
+          builder: (context, state) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('This is the Profile page'),
+                  SizedBox(height: 50),
+                  Text('Another Text')
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
